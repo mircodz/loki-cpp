80ff11667a2a3c1584998928b070457d8868a752
Mirco De Zorzi <mircodezorzi@protonmail.com>
2019-11-10 16:22:50 +0100

    add function to decode chunked responses

--- include/common.hpp
+++ include/common.hpp
@@ -1,8 +1,8 @@
+#ifndef COMMON_HPP_
+#define COMMON_HPP_
+
 #include <vector>
 #include <string>
-
-std::vector<std::string> split(const std::string &str, const std::string &delims);
-
 
 namespace http {
 
@@ -10,6 +10,14 @@ std::string post(std::string_view host, int port, std::string_view path, std::st
 std::string get(std::string_view host, int port, std::string_view path);
 std::string full_get(std::string_view host, int port, std::string_view path);
 
-int get_code(const std::string &response);
+namespace detail {
 
-}
+int get_code(const std::string &response);
+std::string decode_chunked(int sock);
+
+} // namespace detail
+
+} // namespace http
+
+#endif /* COMMON_HPP_ */
+
--- src/common.cpp
+++ src/common.cpp
@@ -1,28 +1,12 @@
 #include "common.hpp"
 
-#include <stdio.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
+#include <cstdio>
+#include <cstring>
+#include <sys/socket.h>
 #include <unistd.h>
-#include <string.h>
 
 #include <fmt/format.h>
-#include <iostream>
-
-std::vector<std::string> split(const std::string &str, const std::string &delims)
-{
-	std::vector<std::string> output;
-	size_t first = 0;
-	while (first < str.size()) {
-		const auto second = str.find_first_of(delims, first);
-		if (first != second)
-			output.emplace_back(str.substr(first, second - first));
-		if (second == std::string::npos)
-			break;
-		first = second + 1;
-	}
-	return output;
-}
 
 namespace http {
 
@@ -37,14 +21,10 @@ std::string post(std::string_view host, int port, std::string_view path, std::st
 		"Content-Length: {}"
 		"\r\n\r\n{}";
 
-	int valread = 0;
-	int sock    = 0;
-
-	struct sockaddr_in serv_addr;
-
-	const std::string request = fmt::format(raw, path, port, host, payload.size(), payload);
-
 	char buffer[1024] = { 0 };
+	const std::string request = fmt::format(raw, path, port, host, payload.size(), payload);
+	int sock = 0;
+	struct sockaddr_in serv_addr{};
 
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 		return "socket creation error";
@@ -59,7 +39,7 @@ std::string post(std::string_view host, int port, std::string_view path, std::st
 		return "connection failed";
 
 	send(sock, request.c_str(), request.size(), 0);
-	valread = read(sock, buffer, sizeof buffer);
+	read(sock, buffer, sizeof buffer);
 
 	return std::string(buffer);
 
@@ -73,14 +53,10 @@ std::string get(std::string_view host, int port, std::string_view path)
 		"Accept: */*\r\n"
 		"\r\n\r\n";
 
-	int valread = 0;
-	int sock    = 0;
-
-	struct sockaddr_in serv_addr;
-
-	const std::string request = fmt::format(raw, path, port, host);
-
 	char buffer[1024] = { 0 };
+	const std::string request = fmt::format(raw, path, port, host);
+	int sock = 0;
+	struct sockaddr_in serv_addr{};
 
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 		return "socket creation error";
@@ -95,7 +71,7 @@ std::string get(std::string_view host, int port, std::string_view path)
 		return "connection failed";
 
 	send(sock, request.c_str(), request.size(), 0);
-	valread = recv(sock, buffer, sizeof buffer, 0);
+	recv(sock, buffer, sizeof buffer, 0);
 
 	return std::string(buffer);
 }
@@ -108,15 +84,9 @@ std::string full_get(std::string_view host, int port, std::string_view path)
 		"Accept: */*\r\n"
 		"\r\n\r\n";
 
-	int valread = 0;
-	int sock    = 0;
-
-	struct sockaddr_in serv_addr;
-
-	std::string response;
-	char buffer[16535] = { 0 };
-
 	const std::string request = fmt::format(raw, path, port, host);
+	int sock = 0;
+	struct sockaddr_in serv_addr{};
 
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 		return "socket creation error";
@@ -132,44 +102,14 @@ std::string full_get(std::string_view host, int port, std::string_view path)
 
 	send(sock, request.c_str(), request.size(), 0);
 
-	std::string b = "";
-	char c[0];
-	int chunk = 0;
-
-	/* for now we can ignore the headers */
-	while (b.find("\r\n\r\n") == std::string::npos) {
-		read(sock, c, 1);
-		b += c[0];
-	}
-
-	do {
-
-		b = "";
-		while (b.find("\r\n") == std::string::npos) {
-			read(sock, c, 1);
-			b += c[0];
-		}
-		read(sock, c, 1);
-		read(sock, c, 1);
-
-		try {
-			chunk = std::stoi(b, nullptr, 16);
-		} catch (...) { break; }
-
-		read(sock, buffer, chunk);
-
-		buffer[chunk] = '\0';
-		response += buffer;
-
-	} while(chunk);
-
-	return response;
+	return detail::decode_chunked(sock);
 }
+namespace detail {
 
 int get_code(const std::string &response)
 {
-	int l = response.find(" ");
-	int r = response.find(" ", l);
+	int l = response.find(' ');
+	int r = response.find(' ', l);
 	int code = 0;
 	try {
 		code = std::stoi(response.substr(l, r));
@@ -177,4 +117,63 @@ int get_code(const std::string &response)
 	return code;
 }
 
+std::string decode_chunked(int sock)
+{
+	// this took me way more time than I expected...
+
+	constexpr int MAX_CHUNK_SIZE = 8096;
+
+	char *s;
+	char buffer[MAX_CHUNK_SIZE + 1] = { 0 };
+	char c[1]; // read character by character into this buffer
+	int chunk_size = 0;
+	int l = 0, r = 0; // start and end of initial chunk size
+	std::string cs = ""; // hold the string representation of `chunk_size`
+	std::string response;
+
+	// discard headers
+	read(sock, buffer, 512);
+
+	s = strstr(buffer, "\r\n\r\n");
+	l = s - buffer;
+	s = strstr(s + 3, "\r\n");
+	r = s - buffer;
+
+	// extract the position of the first chunk size
+	cs = std::string(buffer).substr(l, r - l);
+	response += std::string(buffer).substr(r + 2);
+	chunk_size = std::stoi(cs, nullptr, 16) - response.size();
+	if (chunk_size > MAX_CHUNK_SIZE) return "";
+
+	// read extracted initial chunk size
+	memset(buffer, '\0', sizeof buffer);
+	read(sock, buffer, chunk_size);
+	response += buffer;
+
+	while (chunk_size) {
+		// read next chunk size
+		cs = "";
+		read(sock, c, 1);
+		read(sock, c, 1);
+		while (cs.find("\r\n") == std::string::npos) {
+			read(sock, c, 1);
+			cs += c[0];
+		}
+		chunk_size = std::stoi(cs, nullptr, 16);
+		if (chunk_size > MAX_CHUNK_SIZE) return "";
+
+		// read next chunk
+		int r = 0;
+		while (r < chunk_size) {
+			memset(buffer, '\0', sizeof buffer);
+			r += read(sock, buffer, chunk_size - r);
+			response += buffer;
+		}
+	}
+
+	return response.substr(0, response.size() - 1);
 }
+
+} // namespace detail
+
+} // namespace http
