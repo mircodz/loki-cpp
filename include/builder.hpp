6f80eb764d88ea311754fe1d0e8d9cb77c55c926
Mirco De Zorzi <mircodezorzi@protonmail.com>
2020-10-13 22:51:28 +0200

    refactor(builder): reformat code, add remote host configuration

--- include/builder.hpp
+++ include/builder.hpp
@@ -1,56 +1,59 @@
-#ifndef BUILDER_HPP_
-#define BUILDER_HPP_
+#ifndef LOKI_CPP_BUILDER_HPP_
+#define LOKI_CPP_BUILDER_HPP_
 
 #include "registry.hpp"
 
-namespace loki
-{
+namespace loki {
 
 template <typename T>
-class Builder
-{
-
+class Builder {
 public:
 
-	Builder<T>& Labels(const std::map<std::string, std::string> &labels)
-	{
+	Builder<T>& Labels(const std::map<std::string, std::string> &labels) {
 		labels_ = labels;
 		return *this;
 	}
 
-	Builder<T>& FlushInterval(std::size_t flush_interval)
-	{
+	Builder<T>& FlushInterval(std::size_t flush_interval) {
 		flush_interval_ = flush_interval;
 		return *this;
 	}
 
-	Builder<T>& MaxBuffer(std::size_t max_buffer)
-	{
+	Builder<T>& MaxBuffer(std::size_t max_buffer) {
 		max_buffer_ = max_buffer;
 		return *this;
 	}
 
-	Builder<T>& LogLevel(Level log_level)
-	{
+	Builder<T>& LogLevel(Level log_level) {
 		log_level_ = log_level;
 		return *this;
 	}
 
-	Builder<T>& PrintLevel(Level print_level)
-	{
+	Builder<T>& PrintLevel(Level print_level) {
 		print_level_ = print_level;
 		return *this;
 	}
 
-	Builder<T>& Colorize(Level level, Color color)
-	{
+	Builder<T>& Remote(const std::string &remote_host) {
+		remote_host_ = remote_host;
+		return *this;
+	} 
+
+	Builder<T>& Colorize(Level level, Color color) {
 		colors_[static_cast<int>(level)] = color;
 		return *this;
 	}
 
-	Registry<T> Build()
-	{
-		return {std::move(labels_), flush_interval_, max_buffer_, log_level_, print_level_, colors_};
+	Registry<T> Build() {
+		return 
+			{ std::move(labels_)
+			, flush_interval_
+			, max_buffer_
+			, log_level_
+			, print_level_
+			, remote_host_
+			, colors_
+			};
 	}
 
 private:
@@ -59,8 +62,14 @@ private:
 	std::size_t max_buffer_{10000};
 	Level log_level_{Level::Info};
 	Level print_level_{Level::Disable};
-
-	std::array<Color, 4> colors_{{Color::White, Color::White, Color::White, Color::White}};
+	std::string remote_host_{"127.0.0.1:3100"};
+	std::array<Color, 4> colors_{
+		{ Color::White
+		, Color::White
+		, Color::White
+		, Color::White 
+		}
+	};
 };
 
 template class Builder<AgentJson>;
@@ -68,7 +77,6 @@ template class Builder<AgentJson>;
 template class Builder<AgentProto>;
 #endif
 
-
 } // namespace loki
 
-#endif /* BUILDER_HPP_ */
+#endif /* LOKI_CPP_BUILDER_HPP_ */
